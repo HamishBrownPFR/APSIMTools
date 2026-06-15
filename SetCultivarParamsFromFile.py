df3f6cd96b26f58d9ddec21ae2bf2a93c6b8a95d
Hamish Brown <hamish.brown@plantandfood.co.nz>
2026-06-15 14:43:50 +1200

    merge conflict

--- SetCultivarParamsFromFile.py
+++ SetCultivarParamsFromFile.py
@@ -6,9 +6,9 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.3.4
+#       jupytext_version: 1.18.1
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
